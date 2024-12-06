858cbff91b96e79add02f661cce3ba87d2831ced
fabiconcept <favourajokubi@gmail.com>
2024-12-06 16:29:43 +0100

    Describe the Poll Creation test

--- Practice Projects/voting-system/test/Lock.ts
+++ Practice Projects/voting-system/test/Lock.ts
@@ -19,5 +19,66 @@ describe("Voting Contact", function () {
         it("Should set the correct owner", async function () {
             expect(await votingContact.owner()).to.equal(owner.address);
         });
+    });
+
+    describe("Poll Creation", function () {
+        it("Should create a new poll", async function () {
+            const pollTitle = "Test Poll";
+            const pollId = "1";
+            const pollDuration = 5;
+
+            await votingContact.createNewPoll(pollId, pollTitle, pollDuration);
+            const totalPolls = await votingContact.total_polls();
+
+            expect(totalPolls).to.equal(1);
+        });
+
+        it("Should not create a new poll with an existing poll id", async function () {
+            const pollTitle = "Test Poll";
+            const pollId = "1";
+            const pollDuration = 5;
+
+            await expect(votingContact.createNewPoll(pollId, pollTitle, pollDuration)).to.be.reverted;
+        });
+
+        it("Should not create a new poll with a poll id that is not a number", async function () {
+            const pollTitle = "Test Poll";
+            const pollId = "abc";
+            const pollDuration = 5;
+            
+            await expect(votingContact.createNewPoll(pollId, pollTitle, pollDuration)).to.rejectedWith(
+                TypeError,
+                'invalid BigNumberish string: Cannot convert abc to a BigInt (argument="value", value="abc", code=INVALID_ARGUMENT, version=6.13.4)'
+            );
+        });
+
+        it("Should not create a new poll with a poll id that is not a positive integer", async function () {
+            const pollTitle = "Test Poll";
+            const pollId = -1;
+            const pollDuration = 5;
+            await expect(votingContact.createNewPoll(pollId, pollTitle, pollDuration)).to.rejectedWith(
+                TypeError,
+                `value out-of-bounds (argument="_pollId", value=-1, code=INVALID_ARGUMENT, version=6.13.4)`
+            );
+        });
+        
+        it("Should not create a new poll with a poll duration that is not a positive integer", async function () {
+            const pollTitle = "Test Poll";
+            const pollId = 2;
+            const pollDuration = -1;
+            
+            await expect(votingContact.createNewPoll(pollId, pollTitle, pollDuration)).to.rejectedWith(
+                TypeError,
+                `value out-of-bounds (argument="_duration", value=-1, code=INVALID_ARGUMENT, version=6.13.4)`
+            );
+        });
+
+        it("Should not allow a user to create a new poll if they are not the owner", async function () {
+            const pollTitle = "Test Poll";
+            const pollId = 2;
+            const pollDuration = 5;
+          
+            await expect(votingContact.connect(otherAccount).createNewPoll(pollId, pollTitle, pollDuration)).to.be.reverted;
+        });
     })
 });
